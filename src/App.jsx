2a3b141b54992f5fba32003904a84ac80438caaa
Rahul <rahuljindal1803@gmail.com>
2024-08-04 01:44:03 +0530

    feat: implemented signup page (frontend)

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,20 @@
-import React from "react";
+// import React from "react";
 import Navbar from "./component/Navbar";
-import { Routes, Route } from "react-router-dom"
+import { Routes, Route } from "react-router-dom";
 import Home from "./page/homeLayout";
 import Footer from "./component/Footer";
 import Login from "./component/loginpage/Login";
+import SignUp from "./component/signUp/Signup";
 function App() {
-
   return (
     <>
       <Navbar />
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/login" element={<Login />} />
+        <Route path="/signup" element={<SignUp />} />
       </Routes>
-      <Footer/>
+      <Footer />
     </>
   );
 }
